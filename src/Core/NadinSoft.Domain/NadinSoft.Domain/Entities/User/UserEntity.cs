ba14e03f260be5d0618a4204d804ce2d11e40ebf
Mohammad <mopharaz@gmail.com>
2025-07-13 17:56:40 +0330

    feat: add UserCode and implement Identity models

--- src/Core/NadinSoft.Domain/NadinSoft.Domain/Entities/User/UserEntity.cs
+++ src/Core/NadinSoft.Domain/NadinSoft.Domain/Entities/User/UserEntity.cs
@@ -4,22 +4,30 @@ using NadinSoft.Domain.Entities.Product;
 
 namespace NadinSoft.Domain.Entities.User;
 
-public sealed class UserEntity: IdentityUser<Guid>, IEntity
+public sealed class UserEntity : IdentityUser<Guid>, IEntity
 {
     public DateTime CreatedDate { get; set; }
     public DateTime ModifiedDate { get; set; }
-    
+
     public string FirstName { get; private set; }
     public string LastName { get; private set; }
 
+    public string UserCode { get; private set; }
     private List<ProductEntity> _products = [];
-    
+
     public IReadOnlyList<ProductEntity> Products => _products;
 
     public UserEntity(string firstName, string lastName, string userName, string email) : base(userName)
     {
+        Id = Guid.NewGuid();
         FirstName = firstName;
         LastName = lastName;
         Email = email;
+        UserCode = Guid.NewGuid().ToString("N")[0..7];
     }
+
+    public ICollection<UserRoleEntity> UserRoles { get; set; }
+    public ICollection<UserClaimEntity> UserClaims { get; set; }
+    public ICollection<UserLoginEntity> UserLogins { get; set; }
+    public ICollection<UserTokenEntity> UserTokens { get; set; }
 }
