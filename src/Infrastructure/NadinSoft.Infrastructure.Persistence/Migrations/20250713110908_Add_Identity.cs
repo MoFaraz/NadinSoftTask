e470e7fdc134bce5a7da9f56c33ada245fd56a10
Mohammad <mopharaz@gmail.com>
2025-07-13 18:00:40 +0330

    feat: run add migrations

---
+++ src/Infrastructure/NadinSoft.Infrastructure.Persistence/Migrations/20250713110908_Add_Identity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NadinSoft.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserCode",
+                schema: "usr",
+                table: "Users",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserCode",
+                schema: "usr",
+                table: "Users");
+        }
+    }
+}
