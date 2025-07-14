d7ac1e8edb5da2589a6a5b24260bb591bf378208
Mohammad <mopharaz@gmail.com>
2025-07-14 16:37:31 +0330

    feat: add tests for identity

--- src/Tests/NadinSoft.Identity.Tests/IdentityTests.cs
+++ src/Tests/NadinSoft.Identity.Tests/IdentityTests.cs
@@ -1,12 +1,9 @@
 using FluentAssertions;
 using Mediator;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using NadinSoft.Application.Contracts.User;
 using NadinSoft.Application.Features.User.Commands.Register;
 using NadinSoft.Application.Features.User.Queries.PasswordLogin;
-using NadinSoft.Domain.Entities.User;
-using NadinSoft.Infrastructure.Persistence;
 
 namespace NadinSoft.Identity.Tests;
 
@@ -26,45 +23,25 @@ public class IdentityTests : IClassFixture<IdentityTestSetup>
 
         var sender = _serviceProvider.GetRequiredService<ISender>();
         var registerResult = await sender.Send(testUser);
+        
         registerResult.Result.Should().BeTrue();
     }
 
     [Fact]
     public async Task Getting_AccessToken_Should_Succeed()
     {
+        var testUser = new RegisterUserCommand("test", "test", "test", "qwe123!@#", "test@test.com", "qwe123!@#");
         var sender = _serviceProvider.GetRequiredService<ISender>();
-
-        var testUser = new RegisterUserCommand("test", "test", "TEST3", "qwe123", "test3@test.com", "qwe123");
         await sender.Send(testUser);
 
-        var userManager = _serviceProvider.GetRequiredService<UserManager<UserEntity>>();
-        var user = await userManager.FindByNameAsync("TEST3");
-        user.Should().NotBeNull();
+        var testUserQuery = new UserPasswordLoginQuery(testUser.Username, testUser.Password);
+        var result = await sender.Send(testUserQuery);
 
-        var tokenQuery = new UserPasswordLoginQuery("TEST3", "qwe123");
-        var tokenQueryResult = await sender.Send(tokenQuery);
-
-        tokenQueryResult.Result.Should().NotBeNull();
-        tokenQueryResult.Result!.AccessToken.Should().NotBeEmpty();
+        result.Result!.AccessToken.Should().NotBeEmpty();
     }
 
     [Fact]
     public async Task Registered_User_Should_Exist_In_Database()
-    {
-        var testUser = new RegisterUserCommand("test", "test", "TEST3", "qwe123", "test3@test.com", "qwe123");
-
-        var sender = _serviceProvider.GetRequiredService<ISender>();
-        var userManager = _serviceProvider.GetRequiredService<UserManager<UserEntity>>();
-
-
-        await sender.Send(testUser);
-
-        var user = userManager.FindByNameAsync("TEST3");
-        user.Should().NotBeNull("User should exist in DB before login.");
-    }
-
-    [Fact]
-    public async Task Temp_Test()
     {
         var testUser = new RegisterUserCommand("test", "test", "TEST3", "qwe123", "test3@test.com", "qwe123");
 
@@ -76,11 +53,5 @@ public class IdentityTests : IClassFixture<IdentityTestSetup>
 
         var user = userManager.GetUserByUserNameAsync("TEST3");
         user.Should().NotBeNull("User should exist in DB before login.");
-        
-        var tokenQuery = new UserPasswordLoginQuery("TEST3", "qwe123");
-        var tokenQueryResult = await sender.Send(tokenQuery);
-
-        tokenQueryResult.Result.Should().NotBeNull();
-        tokenQueryResult.Result!.AccessToken.Should().NotBeEmpty();
     }
 }
