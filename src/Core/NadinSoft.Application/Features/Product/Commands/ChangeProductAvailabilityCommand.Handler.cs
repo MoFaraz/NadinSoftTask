089ad7908b52a0f53c3e1b9d94185e0998fb93f6
Mohammad <mopharaz@gmail.com>
2025-07-14 18:56:33 +0330

    feat: only user that created product could change its availability

--- src/Core/NadinSoft.Application/Features/Product/Commands/ChangeProductAvailabilityCommand.Handler.cs
+++ src/Core/NadinSoft.Application/Features/Product/Commands/ChangeProductAvailabilityCommand.Handler.cs
@@ -4,15 +4,22 @@ using NadinSoft.Application.Repositories.Common;
 
 namespace NadinSoft.Application.Features.Product.Commands;
 
-public class ChangeProductAvailabilityCommandHandler(IUnitOfWork unitOfWork): IRequestHandler<ChangeProductAvailabilityCommand, OperationResult<bool>>
+public class ChangeProductAvailabilityCommandHandler(IUnitOfWork unitOfWork)
+    : IRequestHandler<ChangeProductAvailabilityCommand, OperationResult<bool>>
 {
-    public async ValueTask<OperationResult<bool>> Handle(ChangeProductAvailabilityCommand request, CancellationToken cancellationToken)
+    public async ValueTask<OperationResult<bool>> Handle(ChangeProductAvailabilityCommand request,
+        CancellationToken cancellationToken)
     {
         var product = await unitOfWork.ProductRepository.GetProductByIdForUpdateAsync(request.Id, cancellationToken);
-        
+
         if (product is null)
-            return OperationResult<bool>.NotFoundResult(nameof(ChangeProductAvailabilityCommand.Id), "Product not found");
-        
+            return OperationResult<bool>.NotFoundResult(nameof(ChangeProductAvailabilityCommand.Id),
+                "Product not found");
+
+        if (!product.UserId.Equals(request.UserId))
+            return OperationResult<bool>.FailureResult(nameof(ChangeProductAvailabilityCommand.UserId),
+                "User Id does not match");
+
         product.ChangeAvailability(request.IsAvailable);
         await unitOfWork.CommitAsync(cancellationToken);
         return OperationResult<bool>.SuccessResult(true);
--- src/Core/NadinSoft.Application/Features/Product/Commands/ChangeProductAvailabilityCommand.cs
+++ src/Core/NadinSoft.Application/Features/Product/Commands/ChangeProductAvailabilityCommand.cs
@@ -5,12 +5,15 @@ using NadinSoft.Application.Common.Validation;
 
 namespace NadinSoft.Application.Features.Product.Commands;
 
-public record ChangeProductAvailabilityCommand(Guid Id, bool IsAvailable): IRequest<OperationResult<bool>>, IValidatableModel<ChangeProductAvailabilityCommand>
+public record ChangeProductAvailabilityCommand(Guid Id, bool IsAvailable, Guid UserId)
+    : IRequest<OperationResult<bool>>, IValidatableModel<ChangeProductAvailabilityCommand>
 {
-    public IValidator<ChangeProductAvailabilityCommand> Validate(ValidationModelBase<ChangeProductAvailabilityCommand> validator)
+    public IValidator<ChangeProductAvailabilityCommand> Validate(
+        ValidationModelBase<ChangeProductAvailabilityCommand> validator)
     {
         validator.RuleFor(c => c.Id).NotEmpty();
+        validator.RuleFor(c => c.UserId).NotEmpty();
+        validator.RuleFor(c => c.IsAvailable).NotNull();
         return validator;
     }
 }
-    
