26f3f126588fc728555f21ebcaa4e42e8f0e89a9
Mohammad <mopharaz@gmail.com>
2025-07-14 16:42:32 +0330

    feat: configure program.cs

---
+++ src/UI/NadinSoft.Api/Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using NadinSoft.Application.Extensions;
+using NadinSoft.Infrastructure.CrossCutting.Logging;
+using NadinSoft.Infrastructure.Identity.Extensions;
+using NadinSoft.Infrastructure.Persistence.Extensions;
+using NadinSoft.WebFramework.Extensions;
+using NadinSoft.WebFramework.Filters;
+using NadinSoft.WebFramework.Models;
+using NadinSoft.WebFramework.Swagger;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// builder.Host.UseSerilog(LoggingConfiguration.ConfigureLogger);
+
+builder
+    .AddSwagger("v1")
+    .AddVersioning();
+
+builder.Services
+    .AddIdentityServices(builder.Configuration)
+    .AddApplicationAutoMapper()
+    .AddApplicationMediatorServices()
+    .RegisterApplicationValidator()
+    .AddPersistenceDbContext(builder.Configuration);
+
+builder.ConfigureAuthenticationAndAuthorization();
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(typeof(OkResultAttribute));
+    opt.Filters.Add(typeof(NotFoundAttribute));
+    opt.Filters.Add(typeof(BadResultAttribute));
+    opt.Filters.Add(typeof(ModelStateValidationAttribute));
+    opt.Filters.Add(new ProducesResponseTypeAttribute(typeof(ApiResult<Dictionary<string, List<string>>>),
+        StatusCodes.Status400BadRequest));
+
+    opt.Filters.Add(new ProducesResponseTypeAttribute(typeof(ApiResult),
+        StatusCodes.Status401Unauthorized));
+    
+    opt.Filters.Add(new ProducesResponseTypeAttribute(typeof(ApiResult),
+        StatusCodes.Status403Forbidden));
+    
+    opt.Filters.Add(new ProducesResponseTypeAttribute(typeof(ApiResult),
+        StatusCodes.Status500InternalServerError));
+}).ConfigureApiBehaviorOptions(opt =>
+{
+   opt.SuppressModelStateInvalidFilter = true; 
+   opt.SuppressMapClientErrors = true;
+});
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    await app.ApplyMigrationsAsync();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
