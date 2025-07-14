f95dd368f8981b0432fc6645d868447a1d51d11d
Mohammad <mopharaz@gmail.com>
2025-07-14 19:02:19 +0330

    fix: add validation for GetAllProductsQuery.cs

--- src/Core/NadinSoft.Application/Features/Product/Queries/GetAllProductsQuery.cs
+++ src/Core/NadinSoft.Application/Features/Product/Queries/GetAllProductsQuery.cs
@@ -1,6 +1,15 @@
+using FluentValidation;
 using Mediator;
 using NadinSoft.Application.Common;
+using NadinSoft.Application.Common.Validation;
 
 namespace NadinSoft.Application.Features.Product.Queries;
 
-public record GetAllProductsQuery():IRequest<OperationResult<List<GetAllProductsQueryResult>>>;
+public record GetAllProductsQuery() : IRequest<OperationResult<List<GetAllProductsQueryResult>>>,
+    IValidatableModel<GetAllProductsQuery>
+{
+    public IValidator<GetAllProductsQuery> Validate(ValidationModelBase<GetAllProductsQuery> validator)
+    {
+        return validator;
+    }
+}
