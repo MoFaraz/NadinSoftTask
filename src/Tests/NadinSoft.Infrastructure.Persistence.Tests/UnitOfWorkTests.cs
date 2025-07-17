4871c0f494e119fc605355905ac65f4bd99e6190
Mohammad <mopharaz@gmail.com>
2025-07-17 16:26:16 +0330

    feat: write tests for interceptors

--- src/Tests/NadinSoft.Infrastructure.Persistence.Tests/UnitOfWorkTests.cs
+++ src/Tests/NadinSoft.Infrastructure.Persistence.Tests/UnitOfWorkTests.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using FluentAssertions;
 using Mediator;
 using Microsoft.Extensions.DependencyInjection;
@@ -127,5 +128,59 @@ public class UnitOfWorkTests : IClassFixture<PersistenceTestSetup>
 
         editResult.IsSuccess.Should().BeFalse();
         _outputHelper.WriteLineOperationResultErrors(editResult);
+    }
+
+
+    [Fact]
+    public async Task Adding_Product_Should_Set_Created_Date()
+    {
+        var sender = _serviceProvider.GetRequiredService<ISender>();
+        var testUser = new RegisterUserCommand("test", "test", "test", "wsxdr123WS!@", "test@test.com", "wsxdr123WS!@");
+        await sender.Send(testUser);
+
+        var manager = _serviceProvider.GetRequiredService<IUserManager>();
+        var user = await manager.GetUserByUserNameAsync("test", CancellationToken.None);
+
+
+        var product = ProductEntity.Create("product", "09332426728", "mopharaz@gmail.com", DateTime.Now,
+            user!.Id);
+        var context = _serviceProvider.GetRequiredService<NadinSoftDbContext>();
+        context.Add(product);
+        await context.SaveChangesAsync();
+        var entry = context.Entry(product);
+        var createdDate = entry.Property<DateTime>("CreatedDate").CurrentValue;
+        _outputHelper.WriteLine(createdDate.ToString(CultureInfo.InvariantCulture));
+    }
+
+    [Fact]
+    public async Task Editing_Product_Should_Set_Modified_Date()
+    {
+        var sender = _serviceProvider.GetRequiredService<ISender>();
+        var testUser = new RegisterUserCommand("test", "test", "test", "wsxdr123WS!@", "test@test.com", "wsxdr123WS!@");
+        await sender.Send(testUser);
+
+        var manager = _serviceProvider.GetRequiredService<IUserManager>();
+        var user = await manager.GetUserByUserNameAsync("test", CancellationToken.None);
+
+
+        var product = ProductEntity.Create("product", "09332426728", "mopharaz@gmail.com", DateTime.Now,
+            user!.Id);
+        var context = _serviceProvider.GetRequiredService<NadinSoftDbContext>();
+        context.Add(product);
+
+        await context.SaveChangesAsync();
+        var entry = context.Entry(product);
+
+        var editCommand =
+            new EditProductCommand(entry.Entity.Id, "name", "09332426728", "mopharaz@gmail.com", DateTime.Now, user.Id);
+
+        await sender.Send(editCommand);
+        
+        await _unitOfWork.CommitAsync();       
+        
+        var modifiedEntry = context.Entry(product);
+        var modifiedDate = modifiedEntry.Property<DateTime>("ModifiedDate").CurrentValue;
+        
+        _outputHelper.WriteLine(modifiedDate.ToString(CultureInfo.InvariantCulture));
     }
 }
