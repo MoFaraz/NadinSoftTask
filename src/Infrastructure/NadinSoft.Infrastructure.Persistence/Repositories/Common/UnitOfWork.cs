9c76feece517a3d540a97b289e0884f91ffd773f
Mohammad <mopharaz@gmail.com>
2025-07-17 20:17:53 +0330

    feat: implement dispatcher for domain events

--- src/Infrastructure/NadinSoft.Infrastructure.Persistence/Repositories/Common/UnitOfWork.cs
+++ src/Infrastructure/NadinSoft.Infrastructure.Persistence/Repositories/Common/UnitOfWork.cs
@@ -1,16 +1,20 @@
+using Mediator;
 using NadinSoft.Application.Repositories.Common;
 using NadinSoft.Application.Repositories.ProductRepository;
+using NadinSoft.Domain.Common;
 
 namespace NadinSoft.Infrastructure.Persistence.Repositories.Common;
 
 public class UnitOfWork : IUnitOfWork
 {
     private readonly NadinSoftDbContext _db;
+    private readonly IPublisher _publisher;
 
-    public UnitOfWork(NadinSoftDbContext db)
+    public UnitOfWork(NadinSoftDbContext db, IPublisher publisher)
     {
         _db = db;
         ProductRepository = new ProductRepository(_db);
+        _publisher = publisher;
     }
 
     public void Dispose()
@@ -28,5 +32,21 @@ public class UnitOfWork : IUnitOfWork
     public async Task CommitAsync(CancellationToken cancellationToken = default)
     {
         await _db.SaveChangesAsync(cancellationToken);
+
+        await DispatchDomainEventsAsync(cancellationToken);
+    }
+
+    private async Task DispatchDomainEventsAsync(CancellationToken cancellationToken)
+    {
+        var domainEntities = _db.ChangeTracker.Entries<BaseEntity<Guid>>().Where(c => c.Entity.DomainEvents.Count > 0)
+            .ToList();
+
+        var domainEvents = domainEntities.SelectMany(c => c.Entity.DomainEvents).ToList();
+
+        foreach (var entity in domainEntities)
+            entity.Entity.ClearDomainEvents();
+
+        foreach (var domainEvent in domainEvents)
+            await _publisher.Publish(domainEvent, cancellationToken);
     }
 }
